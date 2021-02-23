48b5a4aceddb83956a983da5cac820607ee86544
Reese <rlam2017@gmail.com>
2021-02-22 20:45:08 -0800

    Initial commit

---
+++ api.py
@@ -0,0 +1,80 @@
+import os
+
+from flask import Flask, request, jsonify, render_template
+from flask_migrate import Migrate
+
+from models import Book
+from models import db
+
+app = Flask(__name__)
+
+app.config['APP_SETTINGS'] = os.getenv('APP_SETTINGS')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/tutorial_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/add")
+def add_book():
+    name = request.args.get('name')
+    author = request.args.get('author')
+    published = request.args.get('published')
+    try:
+        book = Book(
+            name=name,
+            author=author,
+            published=published
+        )
+        db.session.add(book)
+        db.session.commit()
+        return "Book added. book id={}".format(book.id)
+    except Exception as e:
+        return str(e)
+
+
+@app.route("/getall")
+def get_all():
+    try:
+        books = Book.query.all()
+        return jsonify([e.serialize() for e in books])
+    except Exception as e:
+        return str(e)
+
+
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    try:
+        book = Book.query.filter_by(id=id_).first()
+        return jsonify(book.serialize())
+    except Exception as e:
+        return str(e)
+
+
+@app.route("/add/form", methods=['GET', 'POST'])
+def add_book_form():
+    if request.method == 'POST':
+        name=request.form.get('name')
+        author=request.form.get('author')
+        published=request.form.get('published')
+        try:
+            book=Book(
+                name=name,
+                author=author,
+                published=published
+            )
+            db.session.add(book)
+            db.session.commit()
+            return "Book added. book id={}".format(book.id)
+        except Exception as e:
+            return(str(e))
+    return render_template("getdata.html")
+
+
+if __name__ == '__main__':
+    app.run()
