e4e91e525d1591fa4528a90ae38d42ab1bdc9d4f
Reese <rlam2017@gmail.com>
2021-02-28 23:43:14 -0800

    merged FlaskBlog code in

--- api.py
+++ api.py
@@ -3,8 +3,13 @@ import os
 from flask import Flask, request, jsonify, render_template
 from flask_migrate import Migrate
 
-from models import Book
-from models import db
+from models import db, Book, Character
+
+
+def get_pc(pc_id):
+    post = db.Query.get(pc_id)
+    return post
+
 
 app = Flask(__name__)
 
@@ -15,9 +20,21 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    posts = []
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/players/<string:pc_id>')
+def player_character(pc_id):
+    pc_page = get_pc(pc_id)
+    return render_template('post.html', post=pc_page)
 
 
 @app.route("/add")
@@ -76,5 +93,69 @@ def add_book_form():
     return render_template("getdata.html")
 
 
+@app.route("/bonk")
+def bonk():
+    return render_template("bonk.html")
+
+
 if __name__ == '__main__':
     app.run()
+
+"""
+@app.route('/<int:post_id>')
+def post(post_id):
+    post_num = get_post(post_id)
+    return render_template('post.html', post=post_num)
+"""
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    # if request.method == 'POST':
+    #     title = request.form['title']
+    #     content = request.form['content']
+    #
+    #     if not title:
+    #         flash('Title is required!')
+    #     else:
+    #         conn = get_db_connection()
+    #         conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+    #                      (title, content))
+    #         conn.commit()
+    #         conn.close()
+    #         return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+"""
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+"""
+
+"""
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
+"""
--- models.py
+++ models.py
@@ -26,3 +26,27 @@ class Book(db.Model):
             'author': self.author,
             'published': self.published
         }
+
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+
+    # id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), primary_key=True)
+    desc = db.Column(db.String())
+    race = db.Column(db.String())
+
+    def __init__(self, name, desc, race):
+        self.name = name
+        self.desc = desc
+        self.race = race
+
+    def __repr__(self):
+        return '<id {}>'.format(self.name)
+
+    def serialize(self):
+        return {
+            'name': self.name,
+            'author': self.desc,
+            'published': self.race
+        }
