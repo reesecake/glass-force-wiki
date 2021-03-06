05ae298db3c24f967fac166732417ef72a071943
Reese <rlam2017@gmail.com>
2021-03-05 21:20:30 -0800

    config and env edits (for safety)

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
-DATABASE_URI = os.getenv('DATABASE_URL')  # 'postgres+psycopg2://postgres:root@localhost:5432/tutorial_db'
+DATABASE_URI = os.getenv('DATABASE_URL')
 
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'secret bonk key'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # APP_SETTINGS = os.environ['APP_SETTINGS']
