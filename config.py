48bbe60eb865d5ebfc4c7ffa4d38398030b7ac99
Reese <rlam2017@gmail.com>
2021-03-07 00:26:16 -0800

    re-added SECRET_KEY :-)

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    SECRET_KEY = 'secret-bonk-key-that-is-really-long-too'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # APP_SETTINGS = os.environ['APP_SETTINGS']
